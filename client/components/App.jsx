3d39dd66e76674d2ec6e15aa664a4c989da9eec7
adam-kuhn <adam.kuhn20@gmail.com>
2018-10-05 12:12:57 +0300

    make a landing page after login before going to profile

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,12 +4,14 @@ import {Route} from 'react-router-dom'
 import Landing from './Landing'
 import Profile from './Profile'
 import Register from './Register'
+import MemberWelcome from './MemberWelcome'
 
 const App = () => {
   return (
     <div>
       <h2>React and Redux development is ready to GOOOO!</h2>
       <Route exact path='/' component={Landing} />
+      <Route path='/members-only/:username' component={MemberWelcome} />
       <Route path='/profile/:username' component={Profile} />
       <Route path='/register' component={Register} />
     </div>
---
+++ client/components/MemberWelcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MemberWelcome = () => {
+  return (
+    <div>
+      <p>Edit Profile</p>
+      <p>Browse cuddle buddies</p>
+    </div>
+  )
+}
+
+export default MemberWelcome
