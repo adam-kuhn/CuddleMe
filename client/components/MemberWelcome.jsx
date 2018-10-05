ec8a8dd5eede558d07828ff5dea5b026d944df85
adam-kuhn <adam.kuhn20@gmail.com>
2018-10-05 12:15:02 +0300

    navigate to edit profile from member landing

--- client/components/MemberWelcome.jsx
+++ client/components/MemberWelcome.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const MemberWelcome = () => {
   return (
     <div>
-      <p>Edit Profile</p>
+      <Link to='/profile/:username'>Edit Profile</Link>
       <p>Browse cuddle buddies</p>
     </div>
   )
